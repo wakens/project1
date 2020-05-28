0310e5e3b3cee1e444e86b394af13013defa8953
wakens <65771520+wakens@users.noreply.github.com>
2020-05-28 14:07:02 -0400

    Update Window.java

--- Window.java
+++ Window.java
@@ -6,38 +6,65 @@ import javax.swing.JOptionPane;
 
 public class Window
 {
+    /*
+     *
+     */
     public void msg(final String msg) {
         JOptionPane.showMessageDialog(null, msg);
     }
-    
+ 
+    /*
+     *
+     */
     public String in(final String msg) {
         return JOptionPane.showInputDialog(msg);
     }
-    
+ 
+    /*
+     *
+     */
     public int option(final String[] options, final String msg) {
         return JOptionPane.showOptionDialog(null, msg, "Click a button", -1, 1, null, options, options[0]);
     }
-    
+ 
+    /*
+     *
+     */
     public void println(final String msg) {
         System.out.println(msg);
     }
-    
+     
+    /*
+     *
+     */
     public void print(final String msg) {
         System.out.println(msg);
     }
-    
+     
+    /*
+     *
+     */
     public void msg(final Object msg) {
         JOptionPane.showMessageDialog(null, msg);
     }
-    
+     
+    /*
+     *
+     */
     public String in(final Object msg) {
         return JOptionPane.showInputDialog(msg);
     }
-    
+     
+    /*
+     *
+     */
     public int option(final Object[] options, final Object msg) {
         return JOptionPane.showOptionDialog(null, msg, "Click a button", -1, 1, null, options, options[0]);
     }
-    
+     
+    /*
+     *
+     */
     public String showInputDialog(final String evaluateQuestion) {
         return null;
     }
