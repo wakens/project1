b74c3017372c353526569d45266b3418ca9e7ef5
wakens <65771520+wakens@users.noreply.github.com>
2020-05-28 20:08:28 -0400

    Update Msg.java

--- Msg.java
+++ Msg.java
@@ -5,10 +5,17 @@ import javax.swing.JOptionPane;
 
 public class Msg
 {
+    /*
+     * This method creates an easy way to display a String message
+     * and ask for user input in JOptionPane.
+     */
     public static String in(final String msg) {
         return JOptionPane.showInputDialog(msg);
     }
-    
+ 
+    /*
+     * This method creates an easy way to display a String message in JOptionPane.
+     */
     public static void msg(final String msg) {
         JOptionPane.showMessageDialog(null, msg);
     }
